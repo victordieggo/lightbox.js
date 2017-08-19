64d502932a7ea461f8d37d760b1f0020cfcfe3b6
Victor Diego <victordieggo@gmail.com>
2017-08-19 15:02:07 -0300

    Smooth scroll adjustments

--- assets/src/js/smooth-scroll.js
+++ assets/src/js/smooth-scroll.js
@@ -5,45 +5,43 @@
 
 (function () {
     'use strict';
-    if ('querySelector' in document && 'addEventListener' in window && Array.prototype.forEach) {
-        var smoothScroll = function (anchor, duration) {
-            var startLocation = window.pageYOffset,
-                endLocation = anchor.offsetTop,
-                distance = endLocation - startLocation,
-                increments = distance / (duration / 16),
-                stopAnimation,
-                runAnimation,
-                animateScroll = function () {
-                    window.scrollBy(0, increments);
-                    stopAnimation();
-                };
-            if (increments >= 0) {
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ((travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight)) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            } else {
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if (travelled <= (endLocation || 0)) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-            runAnimation = setInterval(animateScroll, 16);
-        };
-        Array.prototype.forEach.call(document.querySelectorAll('[data-scroll]'), function (toggle) {
-            toggle.addEventListener('click', function (e) {
-                e.preventDefault();
-                var dataID = toggle.getAttribute('href'),
-                    dataTarget = document.querySelector(dataID),
-                    dataSpeed = toggle.getAttribute('data-speed');
-                if (dataTarget) {
-                    smoothScroll(dataTarget, dataSpeed || 500);
+    function smoothScroll(anchor, duration) {
+        var startLocation = window.pageYOffset,
+            endLocation = anchor.offsetTop,
+            distance = endLocation - startLocation,
+            increments = distance / (duration / 16),
+            stopAnimation,
+            runAnimation,
+            animateScroll = function () {
+                window.scrollBy(0, increments);
+                stopAnimation();
+            };
+        if (increments >= 0) {
+            stopAnimation = function () {
+                var travelled = window.pageYOffset;
+                if ((travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight)) {
+                    clearInterval(runAnimation);
                 }
-            }, false);
-        });
+            };
+        } else {
+            stopAnimation = function () {
+                var travelled = window.pageYOffset;
+                if (travelled <= (endLocation || 0)) {
+                    clearInterval(runAnimation);
+                }
+            };
+        }
+        runAnimation = setInterval(animateScroll, 16);
     }
+    Array.prototype.forEach.call(document.querySelectorAll('[data-scroll]'), function (toggle) {
+        toggle.addEventListener('click', function (e) {
+            e.preventDefault();
+            var dataID = toggle.getAttribute('href'),
+                dataTarget = document.querySelector(dataID),
+                dataSpeed = toggle.getAttribute('data-speed');
+            if (dataTarget) {
+                smoothScroll(dataTarget, dataSpeed || 500);
+            }
+        }, false);
+    });
 }());
